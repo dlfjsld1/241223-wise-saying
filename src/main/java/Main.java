18f93550558ec53fee01de6e5b49967b03c0684d
dlfjsld1 <ltk900216@gmail.com>
2024-12-23 14:46:19 +0900

    1단계 완료

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("== 명언 앱 ==");
+//        System.out.println("명령) 등록");
+//        System.out.println("명언 : 현재를 사랑하라.");
+//        System.out.println("작가 : 작자 미상");
+        System.out.println("명령) 종료");
+    }
+}
