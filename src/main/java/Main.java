d1e848a34f84ec455b848365b3f72b389e1b90a4
dlfjsld1 <ltk900216@gmail.com>
2024-12-23 16:52:49 +0900

    ㅑãif 조건문을 switch로 변경

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -36,32 +36,57 @@ class Quote {
 
 class App {
     private static ArrayList<Quote> dic = new ArrayList<>();
+    private int lastNo = 0;
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
-        int lastNo = 0;
-
-        while(true) {
+        boolean running = true;
+        while(running) {
             System.out.print("명령) ");
             String command = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다림.
-            if(command.equals("종료")) {
-                System.out.println("명언 앱을 종료합니다.");
-                break;
-            } else if (command.equals("등록")) {
-                System.out.print("명언 : ");
-                String saying = scanner.nextLine();
-                System.out.print("작가 : ");
-                String author = scanner.nextLine();
-                Quote quote = new Quote(++lastNo, saying, author);
-                dic.add(quote);
-                System.out.println(lastNo + "번 명언이 등록되었습니다.");
-            } else if (command.equals("목록")) {
-                list();
+            switch(command) {
+                case "종료":
+                    System.out.println("명언 앱을 종료합니다.");
+                    running = false;
+                    break;
+                case "등록":
+                    register();
+                    break;
+                case "목록":
+                    list();
+                    break;
             }
+//            if(command.equals("종료")) {
+//                System.out.println("명언 앱을 종료합니다.");
+//                break;
+//            } else if (command.equals("등록")) {
+//                register();
+//            } else if (command.equals("목록")) {
+//                list();
+//            }
         }
     }
 
+    //마지막에 추가한 번호 추가
+    public void addLastNo() {
+        this.lastNo++;
+    }
+
+    //등록
+    public void register() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("명언 : ");
+        String saying = scanner.nextLine();
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+        addLastNo();
+        Quote quote = new Quote(lastNo, saying, author);
+        dic.add(quote);
+        System.out.println(lastNo + "번 명언이 등록되었습니다.");
+    }
+
+    //목록
     public void list() {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
