2e75808f7128179877cf39dab1a04b5c24801a36
dlfjsld1 <ltk900216@gmail.com>
2024-12-23 15:34:56 +0900

    second complete 3th steps

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -14,11 +16,22 @@ class App {
 
         System.out.println("== 명언 앱 ==");
 
-        System.out.print("명언 : ");
-        String saying = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다림.
-        System.out.print("작가 : ");
-        String author = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다림.
-        System.out.println("1번 명언이 등룍되었습니다.");
-        System.out.println("명령) 종료");
+        while(true) {
+            System.out.print("명령) ");
+            String command = scanner.nextLine();
+            if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                String saying = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다림.
+                System.out.print("작가 : ");
+                String author = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다림.
+                System.out.println("1번 명언이 등룍되었습니다.");
+            } else if(command.equals("종료")) {
+                break;
+            }
+        }
+    }
+
+    public void insert(String saying, String author) {
+
     }
 }
